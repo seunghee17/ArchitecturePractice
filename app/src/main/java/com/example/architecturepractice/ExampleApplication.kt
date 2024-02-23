a7776b66409a1da76eed3a7e67ddb4450b527c10
seunghee17 <victoria9981@naver.com>
2024-02-23 09:20:58 +0900

    init

--- app/src/main/java/com/example/architecturepractice/ExampleApplication.kt
+++ app/src/main/java/com/example/architecturepractice/ExampleApplication.kt
@@ -3,6 +3,7 @@ package com.example.architecturepractice
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//의존성 주입
 @HiltAndroidApp
 class ExampleApplication: Application() {
 }
